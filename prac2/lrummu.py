7e144417455d1b1303f43dde73b567739ef56bcf
Marwan Salih <a1849660@adelaide.edu.au>
2024-09-01 23:45:33 +0930

    mmu try#3

--- prac2/lrummu.py
+++ prac2/lrummu.py
@@ -1,34 +1,55 @@
 from mmu import MMU
+from collections import OrderedDict
 
 class LruMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for LruMMU
-        pass
+        self.frames = frames
+        self.memory = OrderedDict()
+        self.page_faults = 0
+        self.disk_reads = 0
+        self.disk_writes = 0
+        self.debug = False
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        pass
+        self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        pass
+        self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        if page_number in self.memory:
+            self.memory.move_to_end(page_number)
+            if self.debug:
+                print(f"Read page {page_number} from memory.")
+        else:
+            self.page_faults += 1
+            if len(self.memory) >= self.frames:
+                self.memory.popitem(last=False)
+            self.memory[page_number] = True
+            self.disk_reads += 1
+            if self.debug:
+                print(f"Page fault occurred. Read page {page_number} from disk.")
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        if page_number in self.memory:
+            self.memory.move_to_end(page_number)
+            if self.debug:
+                print(f"Write to page {page_number} in memory.")
+        else:
+            self.page_faults += 1
+            if len(self.memory) >= self.frames:
+                self.memory.popitem(last=False)
+            self.memory[page_number] = True
+            self.disk_reads += 1
+            self.disk_writes += 1
+            if self.debug:
+                print(f"Page fault occurred. Write to page {page_number} in disk.")
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
